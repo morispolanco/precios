53cb8b17c579a5ee7588edffbc57faaed597dfc4
morispolanco <morispolanco@gmail.com>
2023-03-29 21:02:34 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,23 +2,23 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-# URL del sitio web del supermercado (reemplace con la URL real)
-url = "https://www.example.com/supermarket"
+# URL del sitio web del supermercado (reemplazar con la URL real)
+url = "https://www.ejemplo.com/supermercado"
 
 # Realizar una solicitud HTTP para obtener el contenido de la página web
-response = requests.get(url)
-soup = BeautifulSoup(response.content, "html.parser")
+respuesta = requests.get(url)
+sopa = BeautifulSoup(respuesta.content, "html.parser")
 
 # Buscar los elementos HTML que contienen la información de los productos y precios
-product_elements = soup.find_all("div", class_="product")
-price_elements = soup.find_all("div", class_="price")
+elementos_productos = sopa.find_all("div", class_="producto")
+elementos_precios = sopa.find_all("div", class_="precio")
 
 # Extraer la información de los productos y precios
-products = [element.text.strip() for element in product_elements]
-prices = [float(element.text.strip().replace("$", "")) for element in price_elements]
+productos = [elemento.text.strip() for elemento in elementos_productos]
+precios = [float(elemento.text.strip().replace("$", "")) for elemento in elementos_precios]
 
 # Crear un DataFrame de Pandas con la información extraída
-data = pd.DataFrame({"product": products, "price": prices})
+datos = pd.DataFrame({"producto": productos, "precio": precios})
 
 # Exportar el DataFrame a un archivo CSV
-data.to_csv("supermarket_prices.csv", index=False)
+datos.to_csv("precios_supermercado.csv", index=False)
