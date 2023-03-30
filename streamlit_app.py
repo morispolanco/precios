c294a9109a2c338aa9bb877d17049bf552006cdd
morispolanco <morispolanco@gmail.com>
2023-03-29 21:28:43 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+from session_state import SessionState
 
 def extraer_precios(url):
     headers = {
@@ -23,15 +24,32 @@ st.title("Extractor de precios de supermercado")
 
 url = st.text_input("Ingrese la URL del supermercado:", "")
 
+session_state = SessionState.get(datos=None, descargar=False)
+
 if url:
     try:
         datos = extraer_precios(url)
         st.write(datos)
+        session_state.datos = datos
 
         if st.button("Descargar en formato XLS"):
-            nombre_archivo = "precios_supermercado.xlsx"
-            datos.to_excel(nombre_archivo, index=False)
-            st.markdown(f"[Descargar archivo]({nombre_archivo})")
+            session_state.descargar = True
+        else:
+            session_state.descargar = False
 
     except Exception as e:
         st.error(f"Error al extraer datos: {e}")
+
+if session_state.descargar and session_state.datos is not None:
+    import base64
+    import io
+
+    output = io.BytesIO()
+    writer = pd.ExcelWriter(output, engine="openpyxl")
+    session_state.datos.to_excel(writer, index=False)
+    writer.save()
+    output.seek(0)
+
+    b64 = base64.b64encode(output.getvalue()).decode()
+    href = f'<a href="data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,{b64}" download="precios_supermercado.xlsx">Descargar archivo XLS</a>'
+    st.markdown(href, unsafe_allow_html=True)
